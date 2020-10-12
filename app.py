4b301daa2dbae0c3f44f650a6d0adccdcb69153d
mkaka182 <mkaka5942@gmail.com>
2020-10-12 09:15:12 +0000

    demo

---
+++ app.py
@@ -0,0 +1,10 @@
+
+import os
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_youtube():
+        target = os.enviorn.get('TARGET', ' YOUTUBE !!')
+        return 'Hello {}!\n'.format(target)
+if __name__ == "_main__":
+        app.run(debug=True,host='0.0.0.0',port=int(os.envioron.get('PORT',8080)))
---
+++ git-push.sh
@@ -0,0 +1,12 @@
+echo "Enter message"
+read message
+git add .
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then 
+ echo "it is clean"
+else
+ git status 
+ echo "Pushing data to remote server!"
+ git push -u origin master
+fi :
